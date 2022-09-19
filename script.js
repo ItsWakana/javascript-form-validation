b7e4947eaac0b58d907cbb42ac90591420a86652
MultiFlex <wakanawardrobe@gmail.com>
2022-09-19 16:07:37 +0100

    add password match check

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ const elements = inputElements();
 submit.addEventListener('click', (event) => {
     event.preventDefault();
     checkValidation.email(elements.email);
+    checkValidation.postcode(elements.postcode);
+    checkValidation.country(elements.country);
+    checkValidation.passwords(elements.password, elements.confirmPassword);
 });
 
 const checkValidation = {
@@ -32,17 +35,43 @@ const checkValidation = {
     },
     checkPatternMismatch(element) {
         if (element.validity.patternMismatch) {
-            element.setCustomValidity('Please enter a valid postcode');
-            element.reportvalidity();
+            element.setCustomValidity('This is not a valid UK postcode');
+            element.reportValidity();
             element.classList.add('inval');
             return;
         }
         element.classList.add('validate');
         element.setCustomValidity('');
     },
+    checkPasswordMatch(pass1, pass2) {
+        if (pass1.value !== pass2.value) {
+            pass1.setCustomValidity('Passwords do not match');
+            pass2.setCustomValidity('Passwords do not match');
+            pass1.reportValidity();
+            pass2.reportValidity();
+            pass1.classList.add('inval');
+            pass2.classList.add('inval');
+            return;
+        }
+        pass1.classList.add('validate');
+        pass2.classList.add('validate');
+        pass1.setCustomValidity('');
+        pass2.setCustomValidity('');
+    },
 
     email(element) {
         this.checkEmptyInput(element);
         this.checkTypeMismatch(element);
+    },
+    postcode(element) {
+        this.checkEmptyInput(element);
+        this.checkPatternMismatch(element);
+    },
+    country(element) {
+        this.checkEmptyInput(element);
+    },
+    passwords(pass1, pass2) {
+        this.checkEmptyInput(pass1, pass2);
+        this.checkPasswordMatch(pass1, pass2);
     }
 }
